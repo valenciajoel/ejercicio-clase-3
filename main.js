0ab041fa0fe004dcbed7a5c7ebdb873489e6e1a7
Joel <valenciajoel0@gmail.com>
2023-04-03 22:30:42 -0300

    Inserto personajes de los simpsons

---
+++ main.js
@@ -0,0 +1,14 @@
+const personajesSimpsons = [
+    { nombre: "Homero", edad: 39, rol: "Padre" },
+    { nombre: "Marge", edad: 36, rol: "Madre" },
+    { nombre: "Bart", edad: 10, rol: "Hijo" },
+    { nombre: "Lisa", edad: 8, rol: "Hija" },
+    { nombre: "Maggie", edad: 1, rol: "Hija" },
+    { nombre: "Ned", edad: 35, rol: "Vecino" },
+    { nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+    { nombre: "Milhouse", edad: 10, rol: "Amigo" },
+    { nombre: "Nelson", edad: 12, rol: "Bravucón" },
+    { nombre: "Martin", edad: 10, rol: "Compañero de clase" }
+    ];
+
+    
